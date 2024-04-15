19a24a14417019f7bbb7fc58dbdbd3f50b2b9b93
ajy14 <emmanuel.ajibade@raoatech.com>
2024-04-15 21:22:17 +0100

    static

--- src/modules/landing/components/NeedHelp.tsx
+++ src/modules/landing/components/NeedHelp.tsx
@@ -1,4 +1,3 @@
-import { formatTimeAgo } from "@/components/formatTimeAgo";
 import { iPostsProps } from "@/types/Posts";
 import {
   Avatar,
@@ -32,80 +31,163 @@ export default function NeedHelp({ posts }: iNeedHelpProps) {
   return (
     <div>
       <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-        {posts?.map((post) => (
-          <GridItem w="100%" key={post.post_id} h={"100%"}>
-            <Link
-              _hover={{
-                textDecoration: "none",
-              }}
-              href={`/posts/${post.post_id}`}
-            >
-              <Flex flexDirection="column">
-                <Box bgColor={"teal"} p={4} pl={6} h="50%">
-                  <VStack align="start" spacing={2} color="white">
-                    <Flex mb={4}>
-                      <Tag size={"sm"}> {post.student?.faculty}</Tag>
-                      <Tag ml={1} size={"sm"}>
-                        {" "}
-                        {post.student?.department}
-                      </Tag>
-                      <Tag ml={1} size={"sm"}>
-                        {post.category}
-                      </Tag>
-                    </Flex>
+        {/* {posts?.map((post) => ( */}
+        <GridItem w="100%" key={1} h={"100%"}>
+          <Link
+            _hover={{
+              textDecoration: "none",
+            }}
+            href={`/posts/1`}
+          >
+            <Flex flexDirection="column">
+              <Box bgColor={"teal"} p={4} pl={6} h="50%">
+                <VStack align="start" spacing={2} color="white">
+                  <Flex mb={4}>
+                    <Tag size={"sm"}> Science</Tag>
+                    <Tag ml={1} size={"sm"}>
+                      {" "}
+                      Mathematics
+                    </Tag>
+                    <Tag ml={1} size={"sm"}>
+                      School Fees
+                    </Tag>
+                  </Flex>
 
-                    <Heading
-                      color={"white"}
-                      as={"h1"}
-                      fontSize="large"
-                      fontWeight="bold"
-                    >
-                      {post.title}
-                    </Heading>
-                  </VStack>
-                </Box>
-                <Box width="100%" py={2} pl={6} bg={"white"} h="50%">
-                  <VStack align="start" spacing={2}>
-                    <Text fontSize="sm" color={"gray.500"}>
-                      Published{" "}
-                      {post?.createdAt === undefined
-                        ? ""
-                        : formatTimeAgo(post?.createdAt)}
-                    </Text>
-                    <HStack
-                      spacing={2}
-                      align="center"
-                      justifyContent="space-between"
-                    >
-                      <HStack spacing={2} align="center">
-                        <Avatar
-                          height={"30px"}
-                          width={"30px"}
-                          name={`${post.student?.first_name} ${post.student?.last_name}`}
-                          src="https://bit.ly/dan-abramov"
-                          size="md"
-                        />
-                        <Text fontSize={"13px"}>
-                          {post.student?.last_name} {post.student?.first_name}
-                        </Text>
-                      </HStack>
+                  <Heading
+                    color={"white"}
+                    as={"h1"}
+                    fontSize="large"
+                    fontWeight="bold"
+                  >
+                    Please I need help to pay for my first semester school fees
+                  </Heading>
+                </VStack>
+              </Box>
+              <Box width="100%" py={2} pl={6} bg={"white"} h="50%">
+                <VStack align="start" spacing={2}>
+                  <Text fontSize="sm" color={"gray.500"}>
+                    Published 3 months ago
+                  </Text>
+                  <HStack
+                    spacing={2}
+                    align="center"
+                    justifyContent="space-between"
+                  >
+                    <HStack spacing={2} align="center">
+                      <Avatar
+                        height={"30px"}
+                        width={"30px"}
+                        name={`Ajibade Emmanuel`}
+                        src="https://bit.ly/dan-abramov"
+                        size="md"
+                      />
+                      <Text fontSize={"13px"}>Ajibade Emmanuel</Text>
                     </HStack>
-                    <Divider color={"gray"} fontWeight={"md"} />
-                    <p className="line-clamp-3 text-sm">{post.content}</p>
+                  </HStack>
+                  <Divider color={"gray"} fontWeight={"md"} />
+                  <p className="line-clamp-3 text-sm">
+                    Dear friends, alumni and community, I am reaching out with a
+                    humble request for your support. I am in need of financial
+                    assistance to fund my first semester school fees at the
+                    university of Lagos . Every contribution, no matter the
+                    size, will make a significant impact on my journey. Your
+                    generosity will not only help me achieve my educational
+                    goals but also empower me to give back to the community in
+                    the future. Thank you for considering and being a part of my
+                    educational journey. Together, we can make dreams come true.
+                  </p>
 
-                    <Flex justify={"flex-end"} mt={4}>
-                      <Box>
-                        <Button size={"xs"} colorScheme="teal">
-                          Read More
-                        </Button>
-                      </Box>
-                    </Flex>
-                  </VStack>
-                </Box>{" "}
-              </Flex>
-            </Link>
-          </GridItem>
-        ))}
+                  <Flex justify={"flex-end"} mt={4}>
+                    <Box>
+                      <Button size={"xs"} colorScheme="teal">
+                        Read More
+                      </Button>
+                    </Box>
+                  </Flex>
+                </VStack>
+              </Box>{" "}
+            </Flex>
+          </Link>
+        </GridItem>
+
+        <GridItem w="100%" key={1} h={"100%"}>
+          <Link
+            _hover={{
+              textDecoration: "none",
+            }}
+            href={`/posts/1`}
+          >
+            <Flex flexDirection="column">
+              <Box bgColor={"teal"} p={4} pl={6} h="50%">
+                <VStack align="start" spacing={2} color="white">
+                  <Flex mb={4}>
+                    <Tag size={"sm"}> Science</Tag>
+                    <Tag ml={1} size={"sm"}>
+                      {" "}
+                      Mathematics
+                    </Tag>
+                    <Tag ml={1} size={"sm"}>
+                      School Fees
+                    </Tag>
+                  </Flex>
+
+                  <Heading
+                    color={"white"}
+                    as={"h1"}
+                    fontSize="large"
+                    fontWeight="bold"
+                  >
+                    Please I need help to pay for my first semester school fees
+                  </Heading>
+                </VStack>
+              </Box>
+              <Box width="100%" py={2} pl={6} bg={"white"} h="50%">
+                <VStack align="start" spacing={2}>
+                  <Text fontSize="sm" color={"gray.500"}>
+                    Published 3 months ago
+                  </Text>
+                  <HStack
+                    spacing={2}
+                    align="center"
+                    justifyContent="space-between"
+                  >
+                    <HStack spacing={2} align="center">
+                      <Avatar
+                        height={"30px"}
+                        width={"30px"}
+                        name={`Ajibade Emmanuel`}
+                        src="https://bit.ly/dan-abramov"
+                        size="md"
+                      />
+                      <Text fontSize={"13px"}>Ajibade Emmanuel</Text>
+                    </HStack>
+                  </HStack>
+                  <Divider color={"gray"} fontWeight={"md"} />
+                  <p className="line-clamp-3 text-sm">
+                    Dear friends, alumni and community, I am reaching out with a
+                    humble request for your support. I am in need of financial
+                    assistance to fund my first semester school fees at the
+                    university of Lagos . Every contribution, no matter the
+                    size, will make a significant impact on my journey. Your
+                    generosity will not only help me achieve my educational
+                    goals but also empower me to give back to the community in
+                    the future. Thank you for considering and being a part of my
+                    educational journey. Together, we can make dreams come true.
+                  </p>
+
+                  <Flex justify={"flex-end"} mt={4}>
+                    <Box>
+                      <Button size={"xs"} colorScheme="teal">
+                        Read More
+                      </Button>
+                    </Box>
+                  </Flex>
+                </VStack>
+              </Box>{" "}
+            </Flex>
+          </Link>
+        </GridItem>
+        {/* ))} */}
       </Grid>
     </div>
   );
